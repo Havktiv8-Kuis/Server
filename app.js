0ebea3fcd16819460af5f7856e730520c0884b54
achramadhans <littleworkshop.id@gmail.com>
2020-04-16 22:39:49 +0700

    tester

--- app.js
+++ app.js
@@ -3,29 +3,28 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const cors = require("cors");
-
+const fs = require('fs')
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
 
 
-let userlist = []
+let userlist = JSON.parse(fs.readFileSync('./userlist.json'))
+let userdata = userlist
 io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on("disconnect", () =>
-  {
-    console.log('a user disconnected');
-  });
-
-  socket.on('user-join', (data) => {
-    userlist.push(data)
-    console.log('new user connected');
-    io.emit('user-join', userlist)
-  })
-
+    console.log('connecting');
+    console.log(userlist)
+    socket.on('user-join', (data) => {
+        userdata.push(data)
+        console.log(data.username);
+        fs.writeFileSync('userlist.json', JSON.stringify(userdata, null, 2))
+        io.emit('user-join', userdata)
+    })
+    socket.on("disconnect", () => {
+        console.log('a user disconnected');
+    });
 });
 
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on :3000');
 });
